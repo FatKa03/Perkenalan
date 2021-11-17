e0c831b05787ea788da2dd6c21a9ce26380c4c80
FatKa03 <94508044+FatKa03@users.noreply.github.com>
2021-11-17 11:29:57 +0700

    Add files via upload

---
+++ perkenalan.py
@@ -0,0 +1,7 @@
+Nama = "Fatikha Kamila Azaria"
+Nim = 212410102029
+Prodi = "Teknologi Informasi"
+Asal = "Pasuruan"
+Univ = "Universitas Jember"
+
+print("Perkenalkan saya", Nama, "dengan NIM", Nim, "dari prodi", Prodi, Univ, ".Saya berasal dari", Asal)
